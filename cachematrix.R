7848418a2593fbca67cac6a73152bc804a836f6f
rkirk822 <rkirk822@gmail.com>
2017-12-17 18:11:41 -0500

    Update

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Set variables x (matrix) and ans (solved matrix) the function's environment 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ans <- NULL #set default for ans
+  set <- function(y) { #define x and ans within current environment
+    x <<- y
+    ans <<- NULL
+  }
+  get <- function() x #returns origninal matrix when called
+  solveit <- function(solved) ans <<- solved #Define ans as solved within current environment
+  issolved <- function() ans # Check if matrix was solved within cachesolve
+  list(set = set, get = get,
+       solveit = solveit,
+       issolved=issolved)
+  
 }
 
 
-## Write a short comment describing this function
+## Check to see if matrix has been solved and solve if no data exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ans <- x$issolved()
+  if(!is.null(ans)) { #Check to see if matrix was solved
+    message("getting cached data")
+    return(ans) # return solved matrix if it exists
+  }
+  data <- x$get() #set matrix as data
+  ans <- solve(data, ...) #solve
+  x$solveit(ans)
+  ans
 }
